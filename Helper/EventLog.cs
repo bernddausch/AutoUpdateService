71b730f3f8a8338f93ce9c77b4914a4423f9c4a8
= <=>
2023-01-03 16:42:06 +0100

    Loggiong Classe Erstellt

---
+++ Helper/EventLog.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    public enum ErrorType
+    {
+        Error,
+        Warning,
+        Information
+    }
+    public class ErrorHandler
+    {
+        private string _logName;
+        private string _logSource;
+
+        public ErrorHandler(string logName, string logSource)
+        {
+            _logName = logName;
+            _logSource = logSource;
+        }
+
+        public void LogError(String msg, ErrorType type)
+        {
+            try
+            {
+                if (!EventLog.SourceExists(_logSource))
+                    EventLog.CreateEventSource(_logSource, _logName);
+
+                if (type == ErrorType.Information)
+                    EventLog.WriteEntry(_logSource, msg, EventLogEntryType.Information, 1);
+                else if (type == ErrorType.Warning)
+                    EventLog.WriteEntry(_logSource, msg, EventLogEntryType.Warning, 1);
+                else
+                    EventLog.WriteEntry(_logSource, msg, EventLogEntryType.Error, 1);
+            }
+            catch { }
+        }
+
+    }
+}
--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -1,8 +1,7 @@
 ﻿using Helper;
 using System;
+using System.IO;
 using System.IO.Pipes;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace TestConsole
 {
@@ -10,26 +9,9 @@ namespace TestConsole
     {
         static void Main(string[] args)
         {
-            Task<apiresponse> job = apiclient.GetJobFromAPIAsync(dnshelper.LookupServices());
-            try
-            {
-                 if (job.Result != null)
-                {
-                    Console.WriteLine(job.Result.Task);
-                    int CurrentTaks = (int)job.Result.Task;
-                    Console.WriteLine(job.Result.Scriptblock);
-                    Thread.Sleep(2000);
-                    psworker worker = new psworker();
-                    //worker.Connect();
-                    worker.RunScriptBlock("Start-SWSoftwareUpdate");
-                    //worker.Close();
-                    apiclient.SendStepToAPI(dnshelper.LookupServices(), CurrentTaks);
-                }
-
-            }
-            catch { }
-
-            Console.ReadLine();
+            Helper.ErrorHandler MyErrors = new Helper.ErrorHandler("SchuWa", "AutoUpdateAgent");
+            MyErrors.LogError("asdfasfd", ErrorType.Warning);
+            MyErrors.LogError("ig", ErrorType.Information);
         }
     }
 }
